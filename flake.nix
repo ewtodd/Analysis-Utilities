429a4673c4138b586e53d565f0bcbed51f8b3f82
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-08-05 11:44:04 -0500

    chore: update flake.lock

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           };
         };
 
-        version = "26.7.23";
+        version = "26.8.5";
 
         rootWithCuda = pkgs.root.overrideAttrs (old: {
           cmakeFlags = (old.cmakeFlags or [ ]) ++ [
